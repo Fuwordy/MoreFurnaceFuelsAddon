6f6e027f0c964722ed4afeb94aff0b7f31166a96
Donovan Bell <Fuwordy@gmail.com>
2015-07-19 18:14:09 -0700

    xD

--- jni/MoreFurnaceFuels/item/BrownCoal.cpp
+++ jni/MoreFurnaceFuels/item/BrownCoal.cpp
@@ -1,7 +1,7 @@
 #include "BrownCoal.h"
 
 BrownCoal* BrownCoal::brownCoalItem = NULL;
-
+//Racist brown coal what is that post to mean.
 BrownCoal::BrownCoal(int itemId) : Item(itemId) {
 	this->setMaxStackSize(64);
 	this->setIcon("brownCoal", 0);
